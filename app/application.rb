a7e9adf82fdce3961062392a9fe828747223f27b
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-05 14:13:11 -0400

    commit tree 1cd48cee9d936bb9e8f261dbd6c991cd6d8e9c39

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,8 @@
+require 'pry'
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -17,6 +19,30 @@ class Application
       resp.write "Path Not Found"
     end
 
+    #responds with empty cart message if the cart is empty
+    #responds with a cart list if there is something in there
+    if req.path.match(/cart/)
+      if @@cart.empty? 
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |cart|
+        resp.write "#{cart}\n"
+        end
+      end
+    end
+
+    #Will add an item that is in the @@items list
+    #Will not add an item that is not in the @@items list
+    if req.path.match(/add/)
+      search = req.params["item"]
+      if @@items.include?(search)
+        @@cart << search
+        resp.write "added #{search}"
+      else
+        resp.write "We don't have that item"
+      end
+    end
+
     resp.finish
   end
 
@@ -27,4 +53,5 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
 end
